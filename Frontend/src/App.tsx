d997179aa454bce5915b3ba54cbe7098acedf106
Matthew Cania <matthewgab24@gmail.com>
2025-02-15 22:14:15 +0800

    initialization commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+
+    <section
+      className='flex w-screen h-screen justify-center items-center text-blue-400 border'
+    >
+      <h1>
+        Hello
+      </h1>
+    </section>
+
+  )
+
+}
+
+export default App
---
+++ Frontend/tailwind.config.ts
@@ -0,0 +1,15 @@
+export default {
+	content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./index.html"],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+	corePlugins: {
+	  preflight: false,
+	},
+	variants: {
+	  extend: {
+		scrollBehavior: ["responsive"],
+	  },
+	},
+  };
