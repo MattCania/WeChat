36fe1218982ac3bd448a62fe64d89abf3f3473b6
Matthew Cania <matthewgab24@gmail.com>
2025-02-26 20:04:06 +0800

    added header parts

--- Frontend/src/partials/CaptureHeader.tsx
+++ Frontend/src/partials/CaptureHeader.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Dropdown from "../components/dropdown";
 
 export default function CaptureHeader() {
@@ -18,13 +19,17 @@ export default function CaptureHeader() {
 
   return (
     <header className='flex h-10 w-full justify-center items-center bg-cyan-600 fixed'>
-      <nav className='flex justify-center items-center h-full w-3/4 gap-32'>
-		<a 
-			className="flex justify-center items-center w-32 h-8 p-2 rounded-md text-white bg-cyan-800"
-			href=""
-		>
-			WeChat
-		</a>
+      <nav className='flex justify-between items-center h-full w-3/4'>
+        <div
+          className='flex justify-start items-center w-1/3 h-full p-2 rounded-md gap-4'
+        >
+        <a
+          className='flex justify-center items-center w-32 h-8 p-2 rounded-md text-cyan-800 text-xl font-bold'
+          href=''
+        >
+          WeChat
+        </a>
+
 
         {dropdownData.map((item, index) => (
           <Dropdown
@@ -33,6 +38,25 @@ export default function CaptureHeader() {
             content={item.content}
           />
         ))}
+        </div>
+
+        <div
+          className='flex justify-end items-center w-1/3 h-full p-2 rounded-md gap-4'
+        >
+          <Link
+						to="/"
+						className="flex justify-center items-center w-32 h-full gap-2 font-medium text-white rounded-lg outline-none hover:bg-cyan-800 hover:text-white transition-all duration-500"
+					>
+						Contact Us
+					</Link>
+					<Link
+						to="/crossroad/register"
+						className="flex justify-center items-center w-32 h-full gap-2 font-bold bg-cyan-800 text-white rounded-full border border-cyan-950 outline-none focus:outline-1 hover:bg-cyan-200 hover:text-cyan-800 hover:outline-white transition-all duration-300"
+					>
+						Try it now
+					</Link>
+        </div>
+
       </nav>
     </header>
   );
